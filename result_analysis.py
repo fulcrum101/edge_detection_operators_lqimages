6728f1328722110ce27490bfabc70e9b96bd9435
mmuffin101 <69482465+fulcrum101@users.noreply.github.com>
2023-09-02 23:08:48 +0300

    update

--- result_analysis.py
+++ result_analysis.py
@@ -14,9 +14,11 @@ def main():
     }
     for al in ['Laplacian', 'Canny', 'Prewitt', 'Sobel', 'Scharr', 'Robert', 'PST']:
         for case in ['blurred', 'scattered', 'noise', 'gamma_w', 'gamma_d', 'm_blurred_v', 'm_blurred_h']:
+            l = 0
             for i in range(5):
                 final_results[al][f'{case}_{i}'] = np.average(data[al][case][i])
-            final_results[al][f'{case}_overall'] = np.average(data[al][case])
+                l += np.average(data[al][case][i])*i
+            final_results[al][f'{case}_overall'] = l/15
     with open('final_results.json', "w") as file:
         json.dump(final_results, file, indent=3)
 
